458e01c95b8fba11466322bf3536b38720f5a2c8
Yesh Raj Singh <114338635+yeshrajs@users.noreply.github.com>
2023-03-13 20:54:36 +0530

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,22 +1,4 @@
 
-$(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
-
-    	if (this.hash !== "") {
-
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
-				window.location.hash = hash;
-			});
-      	} 
-    });
-});
-
 const readMoreBtn = document.querySelector(".read-more-btn");
 const hiddenContent = document.querySelector(".hidden-content");
 readMoreBtn.addEventListener("click", function() {
