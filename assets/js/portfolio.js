548754a7fb3d1af90098287261fd3f280759e444
yeshrajs <singhyeshr@gamil.com>
2023-07-04 19:23:37 +0530

    remove blog section and fixed some bug

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,36 +1,40 @@
-
 // smooth scroll
-$(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
+$(document).ready(function () {
+    $(".nav-link").on('click', function (event) {
 
-    	if (this.hash !== "") {
+        if (this.hash !== "") {
 
-			event.preventDefault();
+            event.preventDefault();
 
-			var hash = this.hash;
+            var hash = this.hash;
 
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
-				window.location.hash = hash;
-			});
-      	} 
+            $('html, body').animate({
+                scrollTop: $(hash)
+                    .offset()
+                    .top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
     });
 });
 // read more
-const readMoreBtn = document.querySelector(".read-more-btn");
+const readMoreBtn = document.getElementById("read-more-btn");
 const hiddenContent = document.querySelector(".hidden-content");
-readMoreBtn.addEventListener("click", function() {
-	hiddenContent.style.display = hiddenContent.style.display === "none" ? "block" : "none";
+ function readmore() {
 
-});
-readMoreBtn.addEventListener("click", function() {
-	if(hiddenContent.style.display === "block" ){
-		readMoreBtn.textContent="SHOW LESS";
-	}
-	else{
-		readMoreBtn.textContent="READ MORE";
-	}
-
-});
-
+    // hiddenContent.style.display = hiddenContent.style.display === "none" ?
+    // "block" : "none";
+    if (hiddenContent.style.display === "none") {
+        hiddenContent.style.display = "block";
+        readMoreBtn.innerHTML = "SHOW LESS";
+    }
+	 else {
+        hiddenContent.style.display = "none";
+     readMoreBtn.innerHTML = "READ MORE";
+    }
+}
+// readMoreBtn.addEventListener("click", function() {
+// if(hiddenContent.style.display === "block" ){ 		readMoreBtn.textContent="SHOW
+// LESS"; 	} 	else{  		readMoreBtn.textContent="READ MORE";  }  }); updated new
+// Read more Button
